d81d2fb09bcf369d85b42a5a37e5817189242ab0
Graham Eichsteadt <GrahamE@ywcanwil.org>
2023-10-09 12:00:47 -0500

    Solutions for module1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Module1-Intro-to-JavaScript
+
+Welcome to JavaScript! Here are the solutions for Module 1 of JavaScript
---
+++ exercises/ex1.js
@@ -0,0 +1,44 @@
+// comment
+// * light green comment
+// ! Warning
+// ? Question
+// TODO 
+
+
+
+// * Do Exercise 1  on the line below. 
+let x = 5;
+document.getElementById("ex1-q1").innerText = x;    // Assign the value of your variable to this line.
+
+// * Do Exercise 2  on the line below.
+let y = 10;
+document.getElementById("ex1-q2").innerText = y;    // Assign the value of your variable to this line.
+
+// * Do Exercise 3  on the line below. 
+let greeting = "Hello world";
+document.getElementById("ex1-q3").innerText = greeting;    // Assign the value of your variable
+
+// * Do Exercise 4  on the line below. 
+document.getElementById("ex1-q4").innerText = x + y;     // * Add x and y here
+
+// * Do Exercise 5  on the line below. 
+let name = 'Graham Eichstaedt';
+document.getElementById("ex1-q5").innerText = name;    // Assign the value of 
+
+// * Do Exercise 6  on the line below. 
+document.getElementById("ex1-q6").innerText = 90 - 100;    // * Subtract 90 from 100 here
+
+//* Do Exercise 7 on the line below.
+document.getElementById("ex1-q7").innerText = 20 * 50;    // * Multiply 20 by 50 here
+
+// * Do Exercise 8 on the line below.
+document.getElementById("ex1-q8").innerText = 100 / 7;   // * Divide 100 by 7
+
+//* Do Exercise 9 on the line below.
+// document.getElementById("ex1-q9").innerText = "Comment out this expression. When you are done, it will no longer display on the webpage";
+
+
+// I am done with Exercise 1
+
+
+
